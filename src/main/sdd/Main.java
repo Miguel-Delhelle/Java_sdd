073baf08b4548368df0396312b9b131a8b880934
Lynnaana <lynnamou@gmail.com>
2025-03-31 17:04:49 +0200

    upd

--- src/main/sdd/Main.java
+++ src/main/sdd/Main.java
@@ -9,15 +9,6 @@ public class Main {
 	public static void main(String[] args) {
 
 		HashSet uneHashSet = new HashSet();
-		TreeSet<IamObject> uneTreeSet = new TreeSet();
-		HashMap uneHashMap = new HashMap();
-		
-		for (int i=0; i< 10000; i++) {
-			IamObject tmp = new IamObject(i,"Je suis l'objet"+i, 3.14);
-			uneTreeSet.add(tmp);
-		}
-		
-		System.out.println(uneTreeSet);
 		
 	}
 	
