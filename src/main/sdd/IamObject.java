71011cb0d65c84ff187f8dbd4072a912b248c0b4
MIGUEL-DELHELLE <miguel.delhelle@gmail.com>
2025-03-31 16:26:30 +0200

    [UPDATE]

---
+++ src/main/sdd/IamObject.java
@@ -0,0 +1,10 @@
+package main.sdd;
+
+public class IamObject {
+	
+	private int id;
+	private String name;
+	private long nbrDeTruc;
+	private float jeSuisUnNombre;
+
+}
---
+++ src/main/sdd/Main.java
@@ -0,0 +1,19 @@
+package main.sdd;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.TreeSet;
+
+public class Main {
+	
+	public static void main(String[] args) {
+
+		HashSet uneHashSet = new HashSet();
+		TreeSet uneTreeSet = new TreeSet();
+		HashMap uneHashMap = new HashMap();
+		
+		
+		
+	}
+
+}
